3e90f630e8a3d87b8370bb5897b19c5cb81488a0
yeshwanth <shwnthr@gmail.com>
2019-12-18 23:40:08 +0530

    Changed the cost calculation from square distance to distance. Improved readability.

--- main.js
+++ main.js
@@ -5,27 +5,23 @@ var config = {
     parent: 'canvasHolder',
     scene: {
         create: create,
-        update: update
     },
     disableContextMenu:true,
 };
-
 var game = new Phaser.Game(config);
-const maxNeibhours = 12;
-const range = 250;
-var graphics;
-// var circle;
-// var mdown = false;
-// var line;
+
 const nodes = [];
 const path = [];
-var activeNode = null;
-var dragged = null;
+const maxNeibhours = 12;
+const range = 16; // Maximum distance between two nodes to be considered neibhours
+var graphics;   // Phaser graphics object
+var dragged = null; // the node that is being dragged by left clicking
 var radius = 20;
+var odometer = -1; // the number of times new node is created
 
 
-var start = null;
-var end = null;
+var startNode = null;   // starting node
+var endNode = null;     // ending node
 
 function Node (px,py) {
     odometer += 1;
@@ -36,26 +32,24 @@ function Node (px,py) {
         neibhours : [],
         text: null,
         from: null,
-        cost: 99999999,
+        gcost: 99999999,
     }
     if(nodes.length == 0) {
-        start = tnode;
+        startNode = tnode;
     }else if(nodes.length == 1){
-        end = tnode;
+        endNode = tnode;
     }
     return tnode;
 }
 var adder;
 function create ()
 {
-    //game.canvas
+    // phaser specific stuff
     graphics = this.add.graphics({ fillStyle: { color: 0xffffff} });
     graphics.lineStyle(4, 0xffdc42,1);
-    // circle = new Phaser.Geom.Circle(250,250, 20);
-    // var line = new Phaser.Geom.Line()
     adder = this.add;
     
-    // starting nodes
+    // default nodes for starting state 
 
     createNode(150,120);
     createNode(650,250)
@@ -66,38 +60,35 @@ function create ()
     repaint();
 
     this.input.on('pointerdown',
-    function (p) {
+        function (p) {
             if(p.middleButtonDown()) {
-            
-                // console.log ('pointer down at '+p.x+','+p.y)
-               addNewNode(p.x,p.y);
-            }
-            if(p.leftButtonDown()){
-                nodes.forEach( n => {
-                    const pointer={x:p.x,y:p.y};
-                    if(sqDist(pointer,n) < radius * radius * Math.PI) {
-                        activeNode = n;
-                        dragged = n;
-                    }
-                });
+                addNewNode(p.x,p.y);
             }
             if(p.rightButtonDown()) { 
                 showPath();
             }
+            if(p.leftButtonDown()){
+                nodes.forEach( n => {
+                    const pointer={x:p.x,y:p.y};
+                    if(Dist(pointer,n) <  radius*2 ) {
+                        dragged = n;
+                    }
+                });
+            }
+        
         }
     )
     this.input.on('pointerup',p => {
         { 
             dragged = null;
-            console.log('pointer up !!!!!!!!!!')
         }
     } );
     this.input.on('pointermove',p => {
         if(dragged){ 
+            // move the node that is being dragged to the pointer location
             dragged.x = p.x;
             dragged.y = p.y;
             repaint();
-            path.splice(0)
             computeNeibhours();
         }
     } );
@@ -107,47 +98,35 @@ function createNode(px,py){
    
     const newNode = Node(px,py)
 
-	let textColor = (newNode == start||newNode==end)?'#ffffff':'#f05f50';
-	//textColor = (newNode == start) ? '#ffffff':textColor;
+    let textColor = (newNode == startNode||newNode==endNode)?'#ffffff':'#f05f50';
+    /*
+     creating and adding a Phaser text element and adding its reference
+     so that we can change its position by dragging
+    */
     newNode.text  = adder.text(
         px-radius*0.4, 
         py-radius*0.5,newNode.name,
         { fontSize: '24px', fill: textColor,fontStyle:'bold'}
     );
-    //newNode.text.style.color= "#ff0000";
 
     nodes.push( newNode );
     computeNeibhours();
     repaint();
 }
 
-function update ()
-{
-    // a += 0.015;
-
-    // graphics.clear();
-    // graphics.fillCircleShape(circle);
-    // graphics.strokeCircleShape(pointerCircle);
-
-
-}
 
 function repaint () {
     graphics.clear();
     graphics.fillStyle(0xffffff);
     graphics.lineStyle(4, 0x666666,1);
-    // graphics.lineBetween(100, 100, 600, 500);
     
     for ( let i = 0;i < nodes.length; i += 1) {
         let c = nodes[i];
         c.text.x = c.x-7;
         c.text.y = c.y-14;
 
-        // console.log(c.name+' has '+c.neibhours.length+' neibhours')
         for ( let j = 0;j < c.neibhours.length; j += 1) {
             const ne = c.neibhours[j].node;
-            // console.log('----'+ne.name)
-            // graphics.lineBetween(ne.x,ne.y,c.x,c.y);
             dx =  (ne.x-c.x)*0.49;
             dy =  (ne.y-c.y)*0.49;
             graphics.lineBetween(c.x,c.y,c.x+dx,c.y+dy);
@@ -157,22 +136,19 @@ function repaint () {
     renderPath();
     for ( let i = 0;i < nodes.length; i += 1) {
         let c = nodes[i];
-        if(start == c){graphics.fillStyle(0x66dd00);}
-        else if(end == c){graphics.fillStyle(0xff0000);}
+        if(startNode == c){graphics.fillStyle(0x66dd00);}
+        else if(endNode == c){graphics.fillStyle(0xff0000);}
         else {graphics.fillStyle(0xffffff);}
         graphics.fillCircle(c.x,c.y,radius);
     }
     
 }
 
-var odometer = -1;
-
-
 
 function uniqueName(odo) {
     const uniques = 26;
     const length = 1 + Math.floor((odo)/uniques);
-    name = '';
+    let name = '';
     for ( let i = 0;i < length; i += 1) {
         const base =  odo % (uniques) ;
         name += String.fromCharCode(97+base);
@@ -181,41 +157,44 @@ function uniqueName(odo) {
     return name;
 }
 
-function sqDist(n1,n2) {
-    return (n2.x-n1.x)**2+(n2.y-n1.y)**2;
+function Dist(n1,n2) {
+    // square distance
+    return Math.sqrt((n2.x-n1.x)**2+(n2.y-n1.y)**2);
 }
 
 
 
 function computeNeibhours(){
+    // function to determine the traversable neibhours of each node
+    path.splice(0);
     for ( let i = 0;i < nodes.length; i += 1) {
         nodes[i].neibhours.splice(0)
     }
     for ( let i = 0;i < nodes.length; i += 1) {
         const curr = nodes [i];
-        var closest = nodes.map( n => {return {node:n,dist:sqDist(n,curr) }} )
-                        .sort( (a,b) => a.dist-b.dist );
-        const maxN = Math.min(closest.length,maxNeibhours+1);
-        let cloMax = null;
+        var nodesByDistance = nodes.map( n => {return {node:n,cost:Dist(n,curr) }} )
+                        .sort( (a,b) => a.cost-b.cost );
+        const nodeCount = Math.min(nodesByDistance.length,maxNeibhours+1);
+        let closest = null;
         // start from 1 cuz closest node to a node is itself
-        for( let a = 1;a < maxN; a++) {
-            const cl = closest[a];
-            if(!cloMax || cloMax.dist>cl.dist){cloMax = cl;}
-            if(cl.dist < range * range) {
+        for( let a = 1;a < nodeCount; a++) {
+            const cl = nodesByDistance[a];
+            if(!closest || closest.cost>cl.cost){closest = cl;}
+            if(cl.cost < range * range) {
                 curr.neibhours.push(cl);
             }
         }
-        let lonely  = curr.neibhours.length == 0 && cloMax!=null;
+        let lonely  = curr.neibhours.length == 0 && closest!=null;
         if(lonely){ 
-            // if(lonely){ console.log(curr);console.log(cl.node);}
-            curr.neibhours.push(cloMax);
-            cloMax.node.neibhours.push({node:curr,dist:sqDist(curr,cloMax.node)});
+            curr.neibhours.push(closest);
+            closest.node.neibhours.push({node:curr,dist:Dist(curr,closest.node)});
         }
     }
 }
 
 
 function renderPath(){
+    // function to draw the yellow path from start node to end node
     graphics.lineStyle(4, 0xffdc42,1);
     for( let i = 0;i < path.length -1; i += 1) {
         const ne = path [i];
@@ -224,61 +203,66 @@ function renderPath(){
     }
 }
 
+const huge = 99999999;
 
-
-const huge =99999999;
+function leastCost(a,b){
+    return a.gcost-b.gcost;
+}
 
 function computePath () {
     if(hasPath()){
-        console.log('has path!! :)')
+        console.log('There is a possible path!! :)')
     }else{
-        alert('there is no possible path !!!')
+        alert('There is no possible path !!!')
         return;
     }
     const completedPool = []
     const activePool = []
-    console.log('computed path')
+    console.log('Computed path :)')
     path.splice(0);
-    if(start && end ){
+    if(startNode && endNode ){
         
         nodes.forEach( n => {
-            n.cost = huge;
+            n.gcost = huge;
             n.from = null;
         });
-        start.cost = 0;
-        activePool.push(start);
+        startNode.gcost = 0;
+        activePool.push(startNode);
 
         while(true){
             const top = activePool[0];
-            if(top)console.log('top = '+top.name);else console.log('top : '+top);
-            console.log(activePool)
-            if(top)console.log(top.neibhours)
+            console.log("*** Top node: "+top.name)
 
             for(let i = 0;i < top.neibhours.length;i += 1){
                 const neibhour = top.neibhours[i];
                 const n = neibhour.node;
                 if(!completedPool.includes(n)){
-                    if(!activePool.includes(n)){activePool.push(n)}
-                    const tcost = sqDist(top,n) + top.cost;
-                    if( tcost < n.cost || n.from == null ){
+                    if(!activePool.includes(n)){activePool.push(n);console.log('pushed '+n.name)}
+                    const costToTop = top.neibhours[i].cost;
+                    const newCost = top.gcost + costToTop;
+                    if( newCost < n.gcost){
+                        console.log('-------')
+                        console.log(n.gcost+' , '+top.gcost+", "+newCost);
                         n.from = top;
-                        n.cost = tcost;
+                        n.gcost = newCost;
+                        console.log(top.name+' initiated '+n.name)
+
                     }
                 }
             }
             completedPool.push(top);
             activePool.sort(leastCost);
-            if(activePool[0]!=end){
+            if(activePool[0]!=endNode){
                 activePool.splice(0,1);
             }else{
                 break;
             }
         }
 
-        curr = end;
+        curr = endNode;
         while(true){
             path.push(curr);
-            if(curr == start ){
+            if(curr == startNode ){
                 break;
             }
             curr = curr.from;
@@ -287,9 +271,6 @@ function computePath () {
     }
 }
 
-function leastCost(a,b){
-    return a.cost-b.cost;
-}
 
 function showPath(){
 	computePath();
@@ -305,31 +286,22 @@ function addNewNode(px=500,py=500){
 function hasPath(){
 
     let doneList = []
-    let tries = 1000;
     let foundEnd =false;
 
     function scanNodes(snode){
-        // console.log('++++++++++')
-        // if(snode)console.log("((((((((((((((((       "+snode.name+"       )))))))))))))")
-        // else console.log(snode);
-        // console.log('--------------------------')
 
         doneList.push(snode.name);
-        if(snode == end ){/*alert('found path');*/foundEnd = true;;return true;}
-        tries += -1;if(tries<0){return false;}
-  
-        // console.log('children = '+snode.neibhours.length)
-        // console.log(snode.neibhours)
+        if(snode == endNode ){foundEnd = true;;return true;}  
+
         for(let i=0;i<snode.neibhours.length;i += 1){
             let n = snode.neibhours[i].node;
             if(!doneList.includes(n.name)){
-                // console.log(doneList)
-                // if(n)console.log(n.name+" >>>>>");                
+                    
                 scanNodes(n);
             }
         }        
     }
 
-    scanNodes(start);
+    scanNodes(startNode);
     return foundEnd;
 }
